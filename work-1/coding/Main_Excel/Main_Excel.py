3471f80827b9a889593d46e1f4f400edf1b44d39
Abhijit Roy <dalvath3700@gmail.com>
2019-05-28 11:21:34 +0530

    Work-1 added

---
+++ work-1/coding/Main_Excel/Main_Excel.py
@@ -0,0 +1,21 @@
+import xlwings as xw
+import pandas as pd
+
+
+def main():
+    # wb = xw.Book.caller()
+    # wb.sheets[0].range("A1").value = "Hello xlwings!"     # test code
+
+    # sht_1 = wb.sheets[0]
+    # sht_run = wb.sheets['RUN']
+
+    excel_file_directory = 'data/sample.xlsx'
+    excel_file = pd.ExcelFile(excel_file_directory)
+    df = excel_file.parse('Table 2', skiprows=0)                            # copy a sheet and paste into another sheet and skiprows 8
+    # sht_run.range('A1').options(index=False).value = df
+    # print(df.columns[0])
+    print(df.loc[[df.columns[0] == 'MENU ITEM']])
+    # print(df)
+
+if __name__ == '__main__':
+    main()
